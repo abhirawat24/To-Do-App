bbf3fefdbf6394bc60d9fdd996082e79f934fd73
Abhishek Rawat <abhishekrawat0324@gmail.com>
2026-09-09 01:39:18 +0530

    Updated functionality

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,15 @@ app.use(express.json());
 //}
 
 app.post("/todo", function (req,res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeparse(createPayload);
+    if (!parsedPayload.success){
+        res.status(411).json({
+            msg:"Wrong inputs",
+        })
+        return;
+    }
+    //put it in mongodb
 
 })
 
@@ -22,5 +31,13 @@ app.get("/todos", function (req, res) {
 })
 
 app.put("/completed", function (req, res) {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeparse(updatePayload);
+    if (!parsedPayload.success){
+        res.status(411).json({
+            msg:"Wrong inputs",
+        })
+        return;
+    }
     
 })
