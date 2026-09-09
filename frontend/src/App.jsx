dad1162a2c895298ff356c866d4a8f9661aab78c
Abhishek Rawat <abhishekrawat0324@gmail.com>
2026-09-10 03:09:50 +0530

    something using cors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,34 @@
 import {useState} from 'react';
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { CreateTodo } from './components/CreateTodo';
 import { Todos } from '../components/Todos';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+   fetch("http://localhost:3000/todos")
+   .then(async function(res) {
+    const json = await res.json();
+    setTodos(json);
+   })
+   
+
+
  return (
   <div>
     <CreateTodo></CreateTodo>
-    <Todos></Todos>
+    <Todos todos={[
+      {
+        title: "Go to gym",
+        description: "You need to go to the gym",
+        completed: false
+      },
+      {
+        title: "Go to school",
+        description: "You need to go to the school",
+        completed: true
+      }
+    ]}></Todos>
   </div>
  ) 
 }
